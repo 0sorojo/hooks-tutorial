306f54b2f8f221e410f2a0b451600999bc739e5a
0sorojo <redrootproductions@gmail.com>
2020-10-16 17:05:02 -0400

    show hide components

--- src/Views/ConditionalRendering.js
+++ src/Views/ConditionalRendering.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CrMultipleRet from '../Components/CR/CrMultipleRet';
 import CrShortCircuit from '../Components/CR/CrShortCircuit';
+import CrShowHide from '../Components/CR/CrShowHide';
 
 const ConditionalRendering = () => {
   return (
     <>
       <CrMultipleRet />
       <CrShortCircuit />
+      <CrShowHide />
     </>
   );
 };
