99f4c83bb26ed730f2379de5c30d63ec1463d76b
0sorojo <redrootproductions@gmail.com>
2020-11-09 14:57:18 -0500

    use Reducer Practice

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ConditionalRendering from './Views/ConditionalRendering';
 import ReactForms from './Views/ReactForms';
 import UseEffectPrac from './Views/UseEffectPrac';
+import UseReducerPrac from './Views/UseReducerPrac';
 import UseRefPrac from './Views/UseRefPrac';
 import USbasics from './Views/UseStateBasics';
 
 const App = () => {
   return (
     <div className='container'>
-      <UseRefPrac />
+      <UseReducerPrac />
     </div>
   );
 };
